475af58deb3ef1d7e49e0fecc00705757e538939
eminekibar <eminekibar.2834@gmail.com>
2025-03-11 00:02:30 +0300

    +

--- push_swap.c
+++ push_swap.c
@@ -14,47 +14,62 @@ void    rr_rrr(t_stack *a, t_stack *b, int *index_a, int *index_b)
 {
     if (*index_a < a->top / 2 && *index_b < b->top / 2)
     {
-        while ((a->top > *index_a && (*index_a)-- >= 0) && (b->top > *index_b && (*index_b)-- >= 0))
+        while ((a->top > *index_a && *index_a >= 0) && (b->top > *index_b && *index_b >= 0))
+        {
             rrr(a, b);  
+            (*index_a)--;
+            (*index_b)--;
+        }
     }
     else if (*index_a >= a->top / 2 && *index_b >= b->top / 2)
     {
-        while ((a->top > *index_a && (*index_a)++ >= 0) && (b->top > *index_b && (*index_b)++ >= 0))
-            rr(a, b);
+        while ((a->top > *index_a && *index_a >= 0) && (b->top > *index_b && *index_b >= 0))
+        {
+            rr(a, b);  
+            (*index_a)++;
+            (*index_b)++;
+        }
     }
 }
 
-void    end_rotation_a(t_stack *a, int index_a)
+void    end_rotation_a(t_stack *a, int *index_a)
 {
-    while (a->top > index_a && index_a >= 0)  
+    if (*index_a < 0)
+        *index_a = a->top;
+    else if (*index_a > a->top)
+        *index_a = 0;
+    while (a->top > *index_a && *index_a >= 0)  
     {
-        if (index_a < a->top / 2)
+        if (*index_a < a->top / 2)
         {
             rra(a);  
-            index_a--;
+            (*index_a)--;
         }
         else
         {  
             ra(a); 
-            index_a++;
+            (*index_a)++;
         } 
     }
-    
 }
 
-void    end_rotation_b(t_stack *b, int index_b)
+void    end_rotation_b(t_stack *b, int *index_b)
 {
-    while (b->top > index_b && index_b >= 0)
+    if (*index_b < 0)
+        *index_b = b->top;
+    else if (*index_b > b->top)
+        *index_b = 0;
+    while (b->top > *index_b && *index_b >= 0)
     {
-        if (index_b < b->top / 2)
+        if (*index_b < b->top / 2)
         {  
             rrb(b);  
-            index_b--;
+            (*index_b)--;
         }
         else
         {
             rb(b);  
-            index_b++;
+            (*index_b)++;
         }
     }
 }
@@ -68,8 +83,8 @@ void    move_to_b(t_stack *a, t_stack *b)
     index_b = find_target_index(b, a->array[index_a]);
     printf("Cheapest: %d [%d]  b target: %d [%d]\n", a->array[index_a], index_a, b->array[index_b], index_b);   //sil
     rr_rrr(a, b, &index_a, &index_b);
-    end_rotation_a(a, index_a);
-    end_rotation_b(b, index_b);
+    end_rotation_a(a, &index_a);
+    end_rotation_b(b, &index_b);
     pb(a, b);
 }
 
